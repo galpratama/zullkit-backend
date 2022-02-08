a28981dc3d2f3799e7894b3a2ea80d1737249619
Galih Pratama <hanamura.iost@gmail.com>
2022-02-08 10:39:10 +0700

    Migrations

---
+++ database/migrations/2022_02_08_031616_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('subtitle');
+            $table->text('description');
+            $table->string('thumbnails')->nullable();
+            $table->boolean('is_figma')->default(true);
+            $table->boolean('is_sketch')->default(true);
+            $table->text('features');
+            $table->integer('price');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
