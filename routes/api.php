4cf7f89f451f27346c938e3cdafb11c9f3bbad34
Galih Pratama <hanamura.iost@gmail.com>
2022-04-05 14:37:11 +0700

    Fix user import

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\CategoryController;
 
